3973e40aa6c47aac5a48383fdbdaecb40b3ea98f
Heisenberg <omaradelawad20013@gmail.com>
2024-01-26 13:32:04 +0200

    set up basic controllers

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const register = async (req, res) => {
+  res.send("Hello");
+};
+
+const login = async (req, res) => {
+  res.send("Hello");
+};
+
+module.exports = {
+  register,
+  login,
+};
---
+++ controllers/jobs.js
@@ -0,0 +1,25 @@
+const getAllJobs = async (req, res) => {
+  res.send("Hello");
+};
+
+const getJob = async (req, res) => {
+  res.send("Hello");
+};
+
+const createJob = async (req, res) => {
+  res.send("Hello");
+};
+const updateJob = async (req, res) => {
+  res.send("Hello");
+};
+const deleteJob = async (req, res) => {
+  res.send("Hello");
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
