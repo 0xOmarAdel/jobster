4228712fc24b3120eabfe6225f7fa5000df99aa7
Heisenberg <omaradelawad20013@gmail.com>
2024-01-27 14:39:43 +0200

    again fixed the jobs routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -13,6 +13,6 @@ router.get("/", getAllJobs);
 router.post("/", createJob);
 router.get("/:id", getJob);
 router.patch("/:id", updateJob);
-router.post("/:id", deleteJob);
+router.delete("/:id", deleteJob);
 
 module.exports = router;
